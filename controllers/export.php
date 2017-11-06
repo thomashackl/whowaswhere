49600350ec1961726ccfef519b5e792dae4dcfd6
hackl <thomas.hackl@uni-passau.de>
2017-11-06 16:07:27 +0100

    correct function signature as inherited

--- controllers/export.php
+++ controllers/export.php
@@ -80,7 +80,7 @@ class ExportController extends AuthenticatedController {
     }
 
     // customized #url_for for plugins
-    public function url_for($to)
+    public function url_for($to = '')
     {
         $args = func_get_args();
 
--- controllers/search.php
+++ controllers/search.php
@@ -147,7 +147,7 @@ class SearchController extends AuthenticatedController {
     }
 
     // customized #url_for for plugins
-    public function url_for($to)
+    public function url_for($to = '')
     {
         $args = func_get_args();
 
