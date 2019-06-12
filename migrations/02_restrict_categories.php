f614e72c75f6a321ff3a04dfe84e2c55024b630b
Thomas Hackl <thomas.hackl@uni-passau.de>
2019-06-12 14:04:59 +0200

    use Config API in migrations

--- migrations/02_restrict_categories.php
+++ migrations/02_restrict_categories.php
@@ -8,20 +8,25 @@
 class RestrictCategories extends Migration {
 
     public function up() {
-        DBManager::get()->execute("INSERT IGNORE INTO `config` VALUES
-            (MD5('WHOWASWHERE_SHOW_COURSE_CATEGORIES'), '', 'WHOWASWHERE_SHOW_COURSE_CATEGORIES', '[1]', 1, 'array',
-                'global', 'whowaswhere', 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(),
-                'Welche Veranstaltungskategorien sollen in der Liste erscheinen? (Standard: Lehre)', '', ''),
-            (MD5('WHOWASWHERE_MATRICULATION_DATAFIELD_ID'), '', 'WHOWASWHERE_MATRICULATION_DATAFIELD_ID', '', 1,
-                'string', 'global', 'whowaswhere', 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(),
-                'ID des freien Datenfelds für die Matrikelnummer (kann auch leer sein, dann wird nichts angezeigt)',
-                '', '')
-        ");
+        Config::get()->create('WHOWASWHERE_SHOW_COURSE_CATEGORIES', [
+            'value' => json_encode([1]),
+            'type' => 'array',
+            'range' => 'global',
+            'section' => 'whowaswhere',
+            'description' => 'Welche Veranstaltungskategorien sollen in der Liste erscheinen? (Standard: Lehre)'
+        ]);
+        Config::get()->create('WHOWASWHERE_MATRICULATION_DATAFIELD_ID', [
+            'value' => '',
+            'type' => 'string',
+            'range' => 'global',
+            'section' => 'whowaswhere',
+            'description' => 'ID des freien Datenfelds für die Matrikelnummer (kann auch leer sein, dann wird nichts angezeigt)'
+        ]);
     }
 
     public function down() {
-        DBManager::get()->execute("DELETE FROM `config` WHERE `field` IN ('WHOWASWHERE_SHOW_COURSE_CATEGORIES',
-            'WHOWASWHERE_MATRICULATION_DATAFIELD_ID')");
+        Config::get()->delete('WHOWASWHERE_SHOW_COURSE_CATEGORIES');
+        Config::get()->delete('WHOWASWHERE_MATRICULATION_DATAFIELD_ID');
     }
 
 }
